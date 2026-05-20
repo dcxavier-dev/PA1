6b08c1fef965d50acbe8fc4e56a37f3d5943f47d
dcxavier-dev <diogocxavier27@gmail.com>
2026-05-20 12:31:35 -0300

    Add files via upload

---
+++ NumerosParesMenor20/src/numerosparesmenor20/NumerosParesMenorVinte.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numerosparesmenor20;
+
+/**
+ *
+ * @author Admin
+ */
+public class NumerosParesMenorVinte {
+
+    
+    public static void main(String[] args) {
+    //Exibir numeros pares entre 1 e 20. (SEM ENTRADA DE DADOS PELO USUARIO)
+    int num=20;
+    for(int i=0; i<num; i=i+2){                             
+        if(i>0){
+            System.out.println("Numeros pares:"+i);
+        }
+    }
+    /*A variavel num se mantém no valor 20 durante todo o processo, intocada.
+    A cada ciclo de execução do bloco for, é somado o valor 2 a i, e o resultado é atribuido a propria variavel i
+    Assim, o bloco for é repetido enquanto i for menor do que a variavel num (que tem valor 20)N
+    */
+        
+        
+    }
+    
+    }
+    
+
