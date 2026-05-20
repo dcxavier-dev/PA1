bb781abc46f1e01e6e57087e79cf611ab33622c7
dcxavier-dev <diogocxavier27@gmail.com>
2026-05-20 12:31:43 -0300

    Add files via upload

---
+++ NumerosExistentes100a125/src/numerosexistentes100a125/NumerosExistentes100a125.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numerosexistentes100a125;
+
+/**
+ *
+ * @author Admin
+ */
+import javax.swing.JOptionPane;
+public class NumerosExistentes100a125 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int V1=100; //Valor menor 
+        int V2=125; //Valor maior
+        int quantia=V2-V1;
+        System.out.println("A quantia de numeros entre "+V1+" e "+V2+" e: "+quantia);
+    }
+    
+}
