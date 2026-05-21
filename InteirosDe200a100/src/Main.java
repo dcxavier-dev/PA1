2da0e2ed401e384129bbcf6936c8188a5678c9b4
dcxavier-dev <diogocxavier27@gmail.com>
2026-05-21 20:46:07 -0300

    Add files via upload

---
+++ InteirosDe200a100/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    void main() {
+        System.out.println("Inteiros de 200 a 100:");
+        for (int i = 200; i>=100; i = i - 1){
+            if(i<=200){
+            System.out.println(+i);
+
+
+            }
+
+
+        }
+    }
+}
